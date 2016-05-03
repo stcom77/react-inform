13c70b786f3ef33b64f4b49b80317208b7481b7a
Adam Nalisnick <theadam4257@gmail.com>
2016-05-03 18:04:11 -0400

    Update createValidate to support async

--- src/createValidate.js
+++ src/createValidate.js
@@ -1,20 +1,48 @@
+const hasThen = obj => !!obj && obj.then instanceof Function;
+
+function gen(obj, index = 0, keys = Object.keys(obj)) {
+  return {
+    keys,
+    index,
+    key: keys[index],
+    value: obj[keys[index]],
+    next: () => gen(obj, index + 1, keys),
+    done: index >= keys.length,
+  };
+}
+
+function validateField(value, values, rule) {
+  if (rule.done) return undefined;
+  const result = rule.value(value, values);
+
+  if (hasThen(result)) {
+    return result.then(res => {
+      if (!res) return rule.key;
+      return validateField(value, values, rule.next());
+    });
+  } else if (!result) return rule.key;
+  return validateField(value, values, rule.next());
+}
+
+
 export default function createValidate(rulesMap) {
   return values => {
     const errors = {};
+    const promises = [];
     Object.keys(rulesMap).forEach(key => {
       const rules = rulesMap[key];
       const value = values[key];
+      const result = validateField(value, values, gen(rules));
 
-      for (const message in rules) {
-        if (rules.hasOwnProperty(message)) {
-          const rule = rules[message];
-          if (!rule(value, values)) {
-            errors[key] = message;
-            break;
+      if (hasThen(result)) {
+        promises.push(result.then(v => {
+          if (v !== undefined) {
+            errors[key] = v;
           }
-        }
-      }
+        }));
+      } else if (result !== undefined) errors[key] = result;
     });
-    return errors;
+    if (promises.length === 0) return errors;
+    return Promise.all(promises).then(() => errors);
   };
 }
